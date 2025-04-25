8d83eb99de443ebf8b9ae98f14267323ef00ce5f
Miguel Lopez <juanjoxdxdxdxdxdxdx@gmail.com>
2025-04-24 22:31:32 -0600

    Cambios de prueba

--- config/conexion.js
+++ config/conexion.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Dracohunter#190603',
-  database: process.env.DB_NAME || 'profesores',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true,
   waitForConnections: true,
   connectionLimit: 10,
